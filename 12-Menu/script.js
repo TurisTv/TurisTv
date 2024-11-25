4153a9c26098ce93069612e6dafa1bfb5b76d84f
TurisTv <mulayimturistv@gmail.com>
2024-11-25 19:26:27 +0100

    Add files via upload

--- 12-Menu/script.js
+++ 12-Menu/script.js
@@ -1,17 +1,9 @@
-$(window).load(function() {
-	
-	  setTimeout(function() {
-		$('.spinner').fadeOut("slow");
-			
-		  setTimeout(function() {
-		  $('#prelaoder').fadeOut("slow"); 
-		  
-			  setTimeout(function() {
-				  $('.content-block').addClass('animated fadeInDown').fadeIn("slow");
-			       $('#footer').fadeIn('slow');
-				   
-			  }, 900);
-		  }, 700);	  
-	  }, 700);	
-  
-});
+/*
+The pure CSS parallax star background has been forked from my other pen :
+
+Parallax Star background in CSS
+https://codepen.io/saransh/details/BKJun
+
+Made by @saran5h
+linkedin.com/in/saranshsinha
+*/
